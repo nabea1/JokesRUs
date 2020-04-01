305315bfd277ae1e9da90f1ef97439697ccf4d79
Nemo <40880374+nabea1@users.noreply.github.com>
2020-04-01 18:12:32 -0500

    Create JokeRepository.java

---
+++ src/main/java/com/galvanize/repositories/JokeRepository.java
@@ -0,0 +1,18 @@
+package com.galvanize.repositories;
+import com.galvanize.entities.Category;
+import com.galvanize.entities.Joke;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface JokeRepository extends JpaRepository<Joke, Long> {
+    List<Joke> findAllByJokeContains(String searchString);
+    List<Joke> findAllByCategory(Category category);
+    @Query(value = "select * from jokes j  where category like ?1 order by RAND() LIMIT 1", nativeQuery = true)
+    Joke findRandomJoke(Category category);
+    @Query(value = "select * from jokes j order by RAND() LIMIT 1", nativeQuery = true)
+    Joke findRandomJoke();
+    @Query(value = "select * from jokes j where category LIKE ?1 order by RAND() LIMIT 1", nativeQuery = true)
+    Joke findRandomJokeByCategory(String category);
+}
